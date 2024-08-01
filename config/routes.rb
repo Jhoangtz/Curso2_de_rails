dce90e0745777010707a7689f2b619779831b0f9
Jhoan Camilo Gutierrez <jhoancagutierrez@gmail.com>
2024-08-01 13:16:41 -0500

    Aprendiendo

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "/products", to: "products#index"
+  get "/products/:id", to: "products#show", as: :product
 end
