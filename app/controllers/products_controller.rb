f06b64d11932f396eaa483fa4d6e3b3579febfc6
Jhoan Camilo Gutierrez <jhoancagutierrez@gmail.com>
2024-09-03 13:03:26 -0500

    Aprendiendo

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,4 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
-  def show 
-    @product = Product.find(params[:id])
-  end
-  def new
-    
-  end
-end
+ 
